29b9ca865da932bf5df9c13dc1c607bb179dc7e8
Jonah Lawrence <jonah@freshidea.com>
2023-09-13 20:58:35 -0600

    New header with dropdowns

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	import './styles.css';
 </script>
 
+<svelte:head>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</svelte:head>
+
 <div class="app">
 	<Header />
 
