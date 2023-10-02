94ea45f24b780d73fa022491c5dfb5dafd9860da
Jonah Lawrence <jonah@freshidea.com>
2023-10-02 11:38:37 -0600

    Refactor query function

--- src/lib/grammars/main.ne
+++ src/lib/grammars/main.ne
@@ -28,12 +28,12 @@ query[QUERY] -> _ $QUERY _ {% data => data[1] %}
 main -> query[unitConversionQuery] {% data => data[0][0] %}
       | query[conversionChartQuery] {% data => data[0][0] %}
 
-unitConversionQuery -> [A-Za-z\s]:* jsonfloat _ unit __ ("to" | "in" | "into") __ unit {% data => ({type: "unitConversionQuery", fromUnit: data[3], toUnit: data[7], amount: data[1]}) %}
-                     | [A-Za-z\s]:* unit __ ("in" | "are in" | "to") __ ("a" | "an") __ unit {% data => ({type: "unitConversionQuery", fromUnit: data[7], toUnit: data[1], amount: 1}) %}
-                     | [A-Za-z\s]:* unit __ ("in" | "are in") __ jsonfloat _ unit {% data => ({type: "unitConversionQuery", fromUnit: data[7], toUnit: data[1], amount: data[5]}) %}
+unitConversionQuery -> [A-Za-z\s]:* jsonfloat _ unit __ ("to" | "in" | "into") __ unit {% data => ({function: "unitConversionQuery", unitFrom: data[3], unitTo: data[7], amount: data[1]}) %}
+                     | [A-Za-z\s]:* unit __ ("in" | "are in" | "to") __ ("a" | "an") __ unit {% data => ({function: "unitConversionQuery", unitFrom: data[7], unitTo: data[1], amount: 1}) %}
+                     | [A-Za-z\s]:* unit __ ("in" | "are in") __ jsonfloat _ unit {% data => ({function: "unitConversionQuery", unitFrom: data[7], unitTo: data[1], amount: data[5]}) %}
 
-conversionChartQuery -> "conversion" __ "chart" __ "for" __ jsonfloat _ unit {% data => ({type: "conversionChartQuery", unit: data[8], amount: data[6]}) %}
-                      | "conversion" __ "chart" __ "for" __ unit {% data => ({type: "conversionChartQuery", unit: data[6], amount: 1}) %}
+conversionChartQuery -> "conversion" __ "chart" __ "for" __ jsonfloat _ unit {% data => ({function: "conversionChartQuery", unit: data[8], amount: data[6]}) %}
+                      | "conversion" __ "chart" __ "for" __ unit {% data => ({function: "conversionChartQuery", unit: data[6], amount: 1}) %}
 
 _ -> [ ]:* {% data => null %}
 
--- src/lib/js/input.js
+++ src/lib/js/input.js
@@ -1,30 +1,16 @@
-import dayjs from 'dayjs';
-import { gregorianToHebrew } from './dateconverter';
-import { isHebrewLeapYear } from './leapyears';
-import { calculateMolad } from './molad';
-import { calculateOmer } from './omer';
 import nearley from 'nearley';
 import grammar from '$lib/grammars/generated/main.cjs';
-import { convertUnits, convertUnitsMulti, convertUnitsMultiAll, getConverters, getOpinion, getOpinions, getUnit } from './unitconverter';
+import { convertUnits, convertUnitsMultiAll, getConverters, getOpinion, getOpinions, getUnit } from './unitconverter';
 
 const INPUT_INTERPRETATION = 'Input Interpretation';
 const RESULT = 'Result';
 
 /**
- * @typedef {Object} InputOptions
- * @property {boolean} [timeFormat12Hour=true] Whether to use 12-hour or 24-hour time format.
- */
-
-/**
  * Respond to a search query using all available tools.
  * @param {string} search The search query.
- * @param {InputOptions} [options] The options.
  * @returns {Promise<{ title: string, content: string }[]>} The response.
  */
-export async function query(search, options = {}) {
-	/** @type {Array<{ title: string, content: string }>} */
-	const sections = [];
-
+export async function query(search) {
 	const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 
 	// convert input to lowercase
@@ -38,89 +24,109 @@ export async function query(search, options = {}) {
 	}
 
 	// get the first result
-	const results = [];
+	const derivations = [];
 
 	// skip over invalid results
-	for (const result of parser.results) {
-		if (result.type === 'unitConversionQuery') {
-			if (result?.fromUnit?.type !== result?.toUnit?.type) {
+	for (const derivation of parser.results) {
+		if (derivation.function === 'unitConversionQuery') {
+			if (derivation?.fromUnit?.type !== derivation?.toUnit?.type) {
 				continue;
 			}
 		}
-		results.push(result);
+		derivations.push(derivation);
 	}
 
 	// TODO: handle multiple interpretations
-	const result = results[0];
+	const derivation = derivations[0];
 
-	// Unit conversion query
-	if (result.type === 'unitConversionQuery') {
-		/** @param {import('./unitconverter').ConversionResult} result */
-		const formatResult = (result) => {
-			// set the precision and remove trailing zeroes
-			const amount = result.result.toFixed(8).replace(/\.?0+$/, '');
-			const resultAmountAndUnit = `${amount} ${result.result === 1 ? unitTo.display : unitTo.displayPlural}`;
-			return result.opinion ? `${resultAmountAndUnit} (${result.opinion})` : resultAmountAndUnit;
-		};
+	/** @type {Record<string, () => Promise<{ title: string, content: string }[]> | { title: string, content: string }[]>} */
+	const sectionFunctions = {
+		"unitConversionQuery": async () => await unitConversionQuery(derivation),
+		"conversionChartQuery": async () => await conversionChartQuery(derivation),
+	};
 
-		let resultValue = '';
-		const unitType = result.fromUnit.type;
-		const unitFrom = await getUnit(unitType, result.fromUnit.unitId);
-		const unitTo = await getUnit(unitType, result.toUnit.unitId);
-		sections.push({ title: INPUT_INTERPRETATION, content: `Convert ${result.amount} ${result.amount === 1 ? unitFrom.display : unitFrom.displayPlural} to ${unitTo.displayPlural}` });
-		const converters = await getConverters();
-		const opinions = getOpinions(converters)[unitType] ?? [];
-		const params = { type: result.fromUnit.type, unitFromId: result.fromUnit.unitId, unitToId: result.toUnit.unitId, amount: result.amount };
-		const conversionResult = await convertUnits(params);
-		resultValue = formatResult(conversionResult);
-		// if there are multiple opinions, show all of them
-		/** @type Array<import('./unitconverter').ConversionResult>*/
-		if (conversionResult.opinion) {
-			const conversionResults = [];
-			for (const opinionId of opinions) {
-				conversionResults.push(await convertUnits({ ...params, opinionId }));
-			}
-			resultValue = conversionResults.map(formatResult).join('\n');
-		}
-		sections.push({ title: RESULT, content: resultValue });
-	}
-	// Conversion Chart query
-	else if (result.type === 'conversionChartQuery') {
-		const unitType = result.unit.type;
-		const unitFrom = await getUnit(unitType, result.unit.unitId);
-		sections.push({ title: INPUT_INTERPRETATION, content: `Show conversion chart for ${unitFrom.displayPlural}` });
-		const params = { type: result.unit.type, unitFromId: result.unit.unitId, amount: result.amount };
-		const conversionResults = await convertUnitsMultiAll(params);
-		// output no-opinion results first
-		let content = '';
-		const noOpinionResults = conversionResults['no-opinion'];
-		if (noOpinionResults) {
-			content += '<ul>';
-			for (const [unitId, result] of Object.entries(noOpinionResults)) {
-				const unitTo = await getUnit(unitType, unitId);
-				content += `<li>${result.result.toFixed(8).replace(/\.?0+$/, '')} ${result.result === 1 ? unitTo.display : unitTo.displayPlural}</li>`;
-			}
-			content += '</ul>';
-		}
-		// output opinion results as a table where the first column is the opinion and the second column is the results for that opinion separated by newlines
-		delete conversionResults['no-opinion'];
-		content += '<table><tr><th>Opinion</th><th>Results</th></tr>';
-		for (const [opinionId, opinionResults] of Object.entries(conversionResults)) {
-			const opinion = await getOpinion(unitType, opinionId);
-			content += `<tr><td>${opinion.name}</td><td><ul>`;
-			for (const [unitId, result] of Object.entries(opinionResults)) {
-				const unitTo = await getUnit(unitType, unitId);
-				content += `<li>${result.result.toFixed(8).replace(/\.?0+$/, '')} ${result.result === 1 ? unitTo.display : unitTo.displayPlural}</li>`;
-			}
-			content += '</ul></td></tr>';
-		}
-		content += '</table>';
-		sections.push({ title: RESULT, content });
-	}
-	// Unsupported query type
-	else {
-		throw new Error(`This type of query is not yet supported: ${result.type}`);
-	}
+	const func = sectionFunctions[derivation.function]();
+	return func instanceof Promise ? await func : func;
+}
 
+/**
+ * Generate sections for a unit conversion query
+ *
+ * @param {{ function: string, unitFrom: { type: string, unitId: string }, unitTo: { type: string, unitId: string }, amount: number }} derivation
+ * @returns {Promise<{ title: string, content: string }[]>} The response.
+ */
+async function unitConversionQuery(derivation) {
+	/** @type {Array<{ title: string, content: string }>} */
+	const sections = [];
+	/** @param {import('./unitconverter').ConversionResult} result */
+	const formatResult = (result) => {
+		// set the precision and remove trailing zeroes
+		const amount = result.result.toFixed(8).replace(/\.?0+$/, '');
+		const resultAmountAndUnit = `${amount} ${result.result === 1 ? unitTo.display : unitTo.displayPlural}`;
+		return result.opinion ? `${resultAmountAndUnit} (${result.opinion})` : resultAmountAndUnit;
+	};
+
+	let resultValue = '';
+	const unitType = derivation.unitFrom.type;
+	const unitFrom = await getUnit(unitType, derivation.unitFrom.unitId);
+	const unitTo = await getUnit(unitType, derivation.unitTo.unitId);
+	sections.push({ title: INPUT_INTERPRETATION, content: `Convert ${derivation.amount} ${derivation.amount === 1 ? unitFrom.display : unitFrom.displayPlural} to ${unitTo.displayPlural}` });
+	const converters = await getConverters();
+	const opinions = getOpinions(converters)[unitType] ?? [];
+	const params = { type: unitType, unitFromId: derivation.unitFrom.unitId, unitToId: derivation.unitTo.unitId, amount: derivation.amount };
+	const conversionResult = await convertUnits(params);
+	resultValue = formatResult(conversionResult);
+	// if there are multiple opinions, show all of them
+	/** @type Array<import('./unitconverter').ConversionResult>*/
+	if (conversionResult.opinion) {
+		const conversionResults = [];
+		for (const opinionId of opinions) {
+			conversionResults.push(await convertUnits({ ...params, opinionId }));
+		}
+		resultValue = conversionResults.map(formatResult).join('\n');
+	}
+	sections.push({ title: RESULT, content: resultValue });
+	return sections;
+}
+
+/**
+ * Generate sections for a conversion chart query
+ *
+ * @param {{ function: string, unit: { type: string, unitId: string }, amount: number }} derivation
+ * @returns {Promise<{ title: string, content: string }[]>} The response.
+ */
+async function conversionChartQuery(derivation) {
+	/** @type {Array<{ title: string, content: string }>} */
+	const sections = [];
+	const unitType = derivation.unit.type;
+	const unitFrom = await getUnit(unitType, derivation.unit.unitId);
+	sections.push({ title: INPUT_INTERPRETATION, content: `Show conversion chart for ${unitFrom.displayPlural}` });
+	const params = { type: unitType, unitFromId: derivation.unit.unitId, amount: derivation.amount };
+	const conversionResults = await convertUnitsMultiAll(params);
+	// output no-opinion results first
+	let content = '';
+	const noOpinionResults = conversionResults['no-opinion'];
+	if (noOpinionResults) {
+		content += '<ul>';
+		for (const [unitId, result] of Object.entries(noOpinionResults)) {
+			const unitTo = await getUnit(unitType, unitId);
+			content += `<li>${result.result.toFixed(8).replace(/\.?0+$/, '')} ${result.result === 1 ? unitTo.display : unitTo.displayPlural}</li>`;
+		}
+		content += '</ul>';
+	}
+	// output opinion results as a table where the first column is the opinion and the second column is the results for that opinion separated by newlines
+	delete conversionResults['no-opinion'];
+	content += '<table><tr><th>Opinion</th><th>Results</th></tr>';
+	for (const [opinionId, opinionResults] of Object.entries(conversionResults)) {
+		const opinion = await getOpinion(unitType, opinionId);
+		content += `<tr><td>${opinion.name}</td><td><ul>`;
+		for (const [unitId, result] of Object.entries(opinionResults)) {
+			const unitTo = await getUnit(unitType, unitId);
+			content += `<li>${result.result.toFixed(8).replace(/\.?0+$/, '')} ${result.result === 1 ? unitTo.display : unitTo.displayPlural}</li>`;
+		}
+		content += '</ul></td></tr>';
+	}
+	content += '</table>';
+	sections.push({ title: RESULT, content });
 	return sections;
 }
