8805d3122d0fb2fbb767f6ab1b9d8d1e34e8bb2f
danielkhayat8 <daniel.khayat8@gmail.com>
2023-10-15 11:54:31 +0300

    Fix 🐛 Bug: The menu does not close automatically on mobile #37

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -54,7 +54,7 @@
 	 * Hide all dropdown menus in the navbar
 	 */
 	function hideDropdowns() {
-		const dropdowns = document.querySelectorAll('.navbar, .dropdown-menu.show, .navbar-col');
+		const dropdowns = document.querySelectorAll('.navbar, .dropdown-menu.show, .navbar-collapse');
 		
 		for (const dropdown of dropdowns) {
 			
