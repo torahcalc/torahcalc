f1dba8677e6e1433414e81462b6ac6b44ef88ba4
Jonah Lawrence <jonah@freshidea.com>
2025-03-30 14:17:49 +0300

    feat: migrate Hebrew Zodiac Calculator UI (#285)
    * feat: migrate Hebrew Zodiac Calculator UI
    
    * style: Formatted code with Prettier
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/routes/tools/hebrew-zodiac-signs/+page.svelte
+++ src/routes/tools/hebrew-zodiac-signs/+page.svelte
@@ -1,43 +1,19 @@
 <script>
-	import { page } from '$app/stores';
-	import dayjs from 'dayjs';
-	import InputCalculator from '../../input/InputCalculator.svelte';
-	import ZodiacExamples from '../../input/examples/ZodiacExamples.svelte';
-
-	/** @type {InputCalculator} */
-	let inputCalculator;
-
-	/** @type {(query: string) => any} The function to call when the button is clicked */
-	$: clickFunction = inputCalculator?.setSections;
-
-	const today = dayjs().format('MMMM D, YYYY');
+	import ZodiacCalculator from './ZodiacCalculator.svelte';
 
 	const description =
 		'Enter your birthday and specify if you were born after sunset. Your full Hebrew birthday and your Hebrew astrology zodiac sign will be displayed (in Hebrew, transliterated Hebrew, and the Latin equivalent).';
-
-	/** @type {string} The current query in the input box (not yet submitted) */
-	export let queryInput = $page.url.searchParams.get('q') ?? `What is the zodiac sign for ${today}?`;
 </script>
 
 <svelte:head>
-	<title>TorahCalc | Hebrew Zodiac Signs</title>
+	<title>TorahCalc | Hebrew Zodiac Sign Calculator</title>
 	<meta name="description" content={description} />
 </svelte:head>
 
 <section>
-	<h1 class="heading">Hebrew Zodiac Signs</h1>
+	<h1 class="heading">Hebrew Zodiac Sign Calculator</h1>
 
 	<p class="center">{description}</p>
 
-	<InputCalculator bind:this={inputCalculator} {queryInput} />
-
-	<div class="examples">
-		<ZodiacExamples {clickFunction} />
-	</div>
+	<ZodiacCalculator />
 </section>
-
-<style>
-	.examples {
-		margin: 0.5rem;
-	}
-</style>
