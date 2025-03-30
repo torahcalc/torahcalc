aeb494249a564afdcafc21c8fc398d4a63224dfc
Jonah Lawrence <jonah@freshidea.com>
2025-03-30 21:49:43 +0300

    feat: Gematria Match frontend

--- src/routes/tools/gematria-match/+page.svelte
+++ src/routes/tools/gematria-match/+page.svelte
@@ -1,18 +1,7 @@
 <script>
-	import { page } from '$app/stores';
-	import InputCalculator from '../../input/InputCalculator.svelte';
-	import GematriaMatchExamples from '../../input/examples/GematriaMatchExamples.svelte';
-
-	/** @type {InputCalculator} */
-	let inputCalculator;
-
-	/** @type {(query: string) => any} The function to call when the button is clicked */
-	$: clickFunction = inputCalculator?.setSections;
+	import GematriaTwoWordMatchCalculator from './GematriaTwoWordMatchCalculator.svelte';
 
 	const description = 'Enter two words and the gematria of each will be calculated according to 25 methods and a list of methods where the words have an equivalent value will be displayed.';
-
-	/** @type {string} The current query in the input box (not yet submitted) */
-	export let queryInput = $page.url.searchParams.get('q') ?? 'Gematria equivalences for תורה and משנה.';
 </script>
 
 <svelte:head>
@@ -25,15 +14,5 @@
 
 	<p class="center">{description}</p>
 
-	<InputCalculator bind:this={inputCalculator} {queryInput} />
-
-	<div class="examples">
-		<GematriaMatchExamples {clickFunction} />
-	</div>
+	<GematriaTwoWordMatchCalculator />
 </section>
-
-<style>
-	.examples {
-		margin: 0.5rem;
-	}
-</style>
